6865c42e573dc9a37f38ac6f1d6febf15153f79b
isaque.s <isaque.error404@gmail.com>
2022-11-10 13:30:07 -0300

    feat: create pages login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,65 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Logo } from "../components/Logo";
+import { auth } from "../services/firebaseConnection";
+
 export function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    if ((email === " ") | (password === "")) {
+      alert("Preencha todos os campos!");
+      return;
+    }
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        toast.success("Bem-vindo de volta! :)");
+        navigate("/admin", { replace: true });
+      })
+      .catch(() => {
+        toast.error("Erro ao tentar fazer o Login!");
+      });
+  }
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-full ">
+      <Logo />
+
+      <form
+        action=""
+        className="flex flex-col w-full max-w-2xl"
+        onSubmit={handleLogin}
+      >
+        <input
+          className="border-0 h-9 mb-4 rounded px-2 outline-none text-gray-500"
+          type="email"
+          placeholder="Digite seu Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          className="border-0  h-9 mb-4 rounded px-2 outline-none text-gray-500"
+          type="password"
+          placeholder="*******"
+          autoComplete="on"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button
+          className="h-9 rounded border-0 bg-blue-300 text-white"
+          type="submit"
+        >
+          Acessar
+        </button>
+      </form>
+    </div>
+  );
 }
