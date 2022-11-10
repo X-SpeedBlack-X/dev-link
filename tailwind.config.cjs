92a0a91c5ec8ec386619efe23c1e51e4df10a5d2
isaque.s <isaque.error404@gmail.com>
2022-11-10 13:28:43 -0300

    styles: add new colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: {
+          400: "#ffd43a",
+          500: "#e18700",
+        },
+        blue: {
+          300: "#3366ff",
+        },
+      },
+    },
   },
   plugins: [],
 };
