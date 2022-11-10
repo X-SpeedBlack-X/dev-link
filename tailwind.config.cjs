a315b09c35f27f44aed73d2f18808ec9d3cff184
isaque.s <isaque.error404@gmail.com>
2022-11-10 16:56:35 -0300

    feat: add animate

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,16 @@ module.exports = {
           300: "#3366ff",
         },
       },
+      animation: {
+        Pop: "animatePop 1s ease-in-out ",
+      },
+
+      keyframes: {
+        animatePop: {
+          "0%": { opacity: 0, transform: "scale(0.7, 0.7)" },
+          "100%": { opacity: 1, transform: "scale(1,1)" },
+        },
+      },
     },
   },
   plugins: [],
