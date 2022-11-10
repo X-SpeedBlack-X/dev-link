189a82acff338e5896146691f987d8d6ef3d9fcd
isaque.s <isaque.error404@gmail.com>
2022-11-10 13:27:07 -0300

    feat: add react-toastify

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { router } from "./App";
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ToastContainer autoClose={1500} />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
