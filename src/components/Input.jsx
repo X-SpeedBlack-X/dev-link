734d56184b51b6654600e63a9a07077ce434c06f
isaque.s <isaque.error404@gmail.com>
2022-11-11 12:37:29 -0300

    feat: add new props

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export function Input({ ...props }) {
+export function Input({ placeholder, type, ...props }) {
   return (
     <input
       className="border-0 h-9 mb-4 rounded px-2 outline-none text-gray-500"
+      placeholder={placeholder}
+      type={type}
       {...props}
     />
   );
